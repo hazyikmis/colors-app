1654ae29f497e41aebde3d78197ca6a631f62b73
Halil Azyikmis <37673859+hazyikmis@users.noreply.github.com>
2020-05-17 03:36:27 +0200

    JSS: Add withStyles with demo styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import MiniPalette from "./MiniPalette";
+
 export default class PaletteList extends Component {
   render() {
     const { palettes } = this.props;
     return (
       <div>
+        <MiniPalette />
         <h1>Palette List</h1>
         {palettes.map((palette) => (
           <p>

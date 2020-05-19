1bed6d41c38af32874d86b2958bb47fae932df10
Halil Azyikmis <37673859+hazyikmis@users.noreply.github.com>
2020-05-19 19:30:37 +0200

    Finish refactor of ColorBox CSS

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -64,6 +64,67 @@ const styles = {
     textDecoration: "none",
     opacity: "0" /* Copy button completely hidden */,
   },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+  },
+  copyOverlay: {
+    opacity: "0",
+    zIndex: "0",
+    width: "100%",
+    height: "100%",
+    transition: "transform 0.6s ease-in-out",
+    transform: "scale(0.1)" /* you might not notice the difference  */,
+  },
+  showOverlay: {
+    opacity: "1",
+    transform: "scale(50)",
+    zIndex: "9",
+    position: "absolute",
+  },
+  copyMsg: {
+    position: "fixed",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    fontSize: "4rem",
+    transform: "scale(0.1)",
+    opacity: "0",
+    color: "white",
+    "& h1": {
+      fontWeight: "400",
+      textShadow: "1px 2px black",
+      background: "rgba(255, 255, 255, 0.2)",
+      width: "100%",
+      textAlign: "center",
+      marginBottom: "0",
+      padding: "1rem",
+      textTransform: "uppercase",
+    },
+    "& p": {
+      fontSize: "2rem",
+      fontWeight: "100",
+    },
+  },
+  showMsg: {
+    opacity: 1,
+    transform: "scale(1)",
+    zIndex: 9,
+    transition: "all 0.4s ease-in-out",
+    transitionDelay: "0.3s",
+  },
 };
 
 class ColorBox extends Component {
@@ -96,17 +157,21 @@ class ColorBox extends Component {
         <div className={classes.colorBox} style={{ background }}>
           {/* The secret div below is the div gets shown hugely and covers the screen when a ColorBox clicked */}
           <div
-            className={`copy-overlay ${copied && "show"}`}
+            //className={`copy-overlay ${copied && "show"}`}
+            className={`${classes.copyOverlay} ${
+              copied && classes.showOverlay
+            }`}
             style={{ background }}
           />
           {/* The another secret div below is the div contains the "Copied" message and color rgb info */}
-          <div className={`copy-msg ${copied && "show"}`}>
+          <div className={`${classes.copyMsg} ${copied && classes.showMsg}`}>
             <h1>Copied!</h1>
             {/* <p className={`${isLightColor && "dark-text"}`}>{background}</p> */}
             <p className={classes.copyText}>{background}</p>
           </div>
-          <div className="copy-container">
-            <div className="box-content">
+          <div>
+            {/* <div className="box-content"> */}
+            <div className={classes.boxContent}>
               {/* <span className={isDarkColor && "light-text"}>{name}</span> */}
               <span className={classes.colorName}>{name}</span>
             </div>

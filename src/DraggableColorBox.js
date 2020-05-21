7c866a65e2139c013b8a27ba7d315538563f91f2
Halil Azyikmis <37673859+hazyikmis@users.noreply.github.com>
2020-05-21 20:01:23 +0200

    Style DraggableColorBox

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { withStyles } from "@material-ui/styles";
 
+import DeleteIcon from "@material-ui/icons/Delete";
+
 const styles = {
   root: {
     width: "20%",
@@ -11,6 +13,30 @@ const styles = {
     position: "relative",
     cursor: "pointer",
     marginBottom: "-4px" /* this is required after styling ".see-more" */,
+    "&:hover svg": {
+      //this styles applied only to the root(DraggableColorBox), when hovered the DraggableColorBox
+      //but we want that these styles should be applied to the deleteIcon - but deleteIcon is a dynamic class name like deleteIcon-2939-232
+      //if we check the deleteIcon in more detail from the F12/Elements, we see that its an "svg"
+      //so, we can use this... to make a trick
+      color: "white",
+      transform: "scale(1.5)",
+    },
+  },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "rgba(0,0,0, 0.5)",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  deleteIcon: {
+    transition: "all 0.3s ease-in-out",
   },
 };
 
@@ -18,7 +44,10 @@ const DraggableColorBox = (props) => {
   const { classes } = props;
   return (
     <div className={classes.root} style={{ backgroundColor: props.color }}>
-      <h4>{props.name}</h4>
+      <div className={classes.boxContent}>
+        <span>{props.name}</span>
+        <DeleteIcon className={classes.deleteIcon} />
+      </div>
     </div>
   );
 };

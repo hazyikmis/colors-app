12ce879830affcc4b90780c9190ebca43d1980c4
Halil Azyikmis <37673859+hazyikmis@users.noreply.github.com>
2020-05-26 18:07:04 +0200

    add DeleteIcon to MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -3,6 +3,8 @@ import { withStyles } from "@material-ui/core/styles";
 
 import styles from "./styles/MiniPaletteStyles";
 
+import DeleteIcon from "@material-ui/icons/Delete";
+
 //The styles below moved to a separate file src/MiniPaletteStyles.js AND imported here as "styles"
 /*
 const styles = {
@@ -26,6 +28,13 @@ function MiniPalette(props) {
   ));
   return (
     <div className={classes.root} onClick={props.handleClick}>
+      <div className={classes.delete}>
+        <DeleteIcon
+          className={classes.deleteIcon}
+          //inline style used for overcoming css specifity problem
+          style={{ transition: "all 0.3s ease-in-out" }}
+        />
+      </div>
       <div className={classes.colors}>
         {/* MINI COLOR BOXES */}
         {miniColorBoxes}
